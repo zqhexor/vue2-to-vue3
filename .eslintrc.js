975911f8a86856363bd575f0bb6b341dc64f45b3
HexOr <zengqiao@asiainfo.com>
2022-03-23 02:35:21 +0800

    @HexOr 适合vue3的eslint检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-v-model-argument': 'off', // v-model后面不能接参数
+    'vue/no-multiple-template-root': 'off', // vue3支持多个根节点
+    'vue/no-v-for-template-key': 'off', // vue3 v-for的key需放在template上
+    'import/first': 'off' // 解决多个script标签都有import
   }
 }
